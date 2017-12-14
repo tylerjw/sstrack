13d0bba25143ac7db976cdcf2d9b95826691c2d3
Tyler Weaver <tweaver@lgsinnovations.com>
2017-12-14 10:50:58 -0700

    scaled everything the same and added course delta

--- ssparse.py
+++ ssparse.py
@@ -6,6 +6,20 @@ from geo.sphere import bearing
 import matplotlib.pyplot as plt
 from math import cos,sin,radians
 
+def calculate_delta(vector, offset=1):
+  return [b-a for a,b in zip(vector[:-offset], vector[offset:])] + [0]*(offset-1)
+
+def upwrap_angles(angles, threshold=180):
+  offset = 0
+  for i in range(1, len(angles)):
+    angles[i] += offset
+    if (angles[i] - angles[i-1] > threshold):
+      offset -= 360
+      angles[i] += offset
+    elif (angles[i-1] - angles[i] > threshold):
+      offset += 360
+      angles[i] += offset
+  return angles
 
 def main():
   usage = "usage: %prog [options] logFile outputFile"
@@ -39,43 +53,52 @@ def main():
         print("Invalid NMEA string: ", msg)
 
   # all distances are in km, convert to m
-  distance_bearing = []
-  speeds = [gps_msgs[0].spd_over_grnd]
+  distances = []
+  bearings = []
+  courses = []
+  speeds = []
   points = [(0,0)]
   a = gps_msgs[0]
   for b in gps_msgs[1:]:
-    x = (a.latitude, a.longitude)
-    y = (b.latitude, b.longitude)
-    p = (distance(x,y),bearing(x,y))
-
-    if (b.spd_over_grnd < 2 or p[0] < 1):
+    if (b.spd_over_grnd < 1):
       continue
 
-    speeds.append(b.spd_over_grnd)
-    distance_bearing.append(p)
+    x = (a.latitude, a.longitude)
+    y = (b.latitude, b.longitude)
+    r = distance(x,y)
+    q = bearing(x,y)
+
+    speeds.append(a.spd_over_grnd)
+    distances.append(r)
+    bearings.append(r)
+    courses.append(a.true_course)
     start = points[-1]
-    end = (start[0]+p[0]*cos(radians(p[1])), start[1]-p[0]*sin(radians(p[1])))
+    end = (start[0]+r*cos(radians(q)), start[1]-r*sin(radians(q)))
     points.append(end)
     a = b
 
-  distances,bearings = zip(*distance_bearing)
-  bearing_delta = [b-a for a,b in zip(bearings[:-1], bearings[1:])]
+  courses = upwrap_angles(courses)
+  course_delta = calculate_delta(courses, 3)
   distance_y = [0]
-  for dist in distances[:-1]:
+  for dist in distances:
     last = distance_y[-1]
     distance_y.append(dist+last)
   distance_y = distance_y[1:]
 
   plt.figure(1)
-  plt.subplot(311)
+  plt.subplot(411)
   x,y = zip(*points)
-  plt.plot(x,y,'bo-')
+  plt.plot(x,y,'-gD', markevery=[0])
 
-  plt.subplot(312)
-  plt.plot(distance_y, bearing_delta, 'x-')
+  plt.subplot(412)
+  plt.axhline()
+  plt.plot(distance_y[:-1], course_delta, 'x-')
 
-  plt.subplot(313)
-  plt.plot(speeds,'r',distances,'k')
+  plt.subplot(413)
+  plt.plot(distance_y,speeds,'r',distance_y,distances,'k')
+
+  plt.subplot(414)
+  plt.plot(distance_y,courses)
   plt.show()
 
 
